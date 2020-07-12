0fbd74da5af2fc478f973f5b88ff6d1f2c4d8479
Edward Hong <edward.hong527@gmail.com>
2020-07-12 21:13:59 +1200

    scroll animation for about

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,3 +1,4 @@
+import useScrollPosition from '@react-hook/window-scroll'
 import styled from 'styled-components'
 
 import { PRIMARY_COLOR } from '../constants'
@@ -11,6 +12,8 @@ const Landing = styled.div`
   width: calc(100vw - (100vw - 100%));
   margin: 0px;
   background-color: ${PRIMARY_COLOR};
+  top: 0;
+  position: ${({ inRange }) => (inRange ? 'fixed' : 'static')};
 
   @media screen and (min-width: 992px) {
     width: calc(50vw - ((100vw - 100%) / 2));
@@ -63,75 +66,89 @@ const TechList = styled.ul`
   }
 `
 
-const About = () => (
-  <Wrapper id="about">
-    <Landing>
-      <Heading>
-        <img
-          src="https://res.cloudinary.com/avatarhzh/image/upload/v1521343685/portfolio/about.svg"
-          alt="About"
-        />
-      </Heading>
-    </Landing>
-    <SectionInfo>
-      <p>
-        I am a full stack Javascript developer from Wellington, New Zealand.
-      </p>
-      <h3>Tech Stack</h3>
-      <p>
-        I specialise in web development with React and usually work with the
-        MERN stack or React with GraphQL
-      </p>
-      <h3>List of technologies I'm familiar with include:</h3>
-      <TechList>
-        <li>
-          <span>HTML5 and CSS3</span>
-        </li>
-        <li>
-          <span>Git and Github</span>
-        </li>
-        <li>
-          <span>Bootstrap, Material-UI and Semantic UI</span>
-        </li>
-        <li>
-          <span>Sass/Scss, CSS-in-JS and styled-components</span>
-        </li>
-        <li>
-          <span>Javascript and jQuery</span>
-        </li>
-        <li>
-          <span>RxJS</span>
-        </li>
-        <li>
-          <span>Node</span>
-        </li>
-        <li>
-          <span>React</span>
-        </li>
-        <li>
-          <span>Redux, Redux-Thunk and Redux-Observables</span>
-        </li>
-        <li>
-          <span>Next</span>
-        </li>
-        <li>
-          <span>Express3</span>
-        </li>
-        <li>
-          <span>GraphQL, Apollo and Prisma</span>
-        </li>
-        <li>
-          <span>MongoDB and Mongoose</span>
-        </li>
-        <li>
-          <span>PostgreSQL</span>
-        </li>
-        <li>
-          <span>Redis</span>
-        </li>
-      </TechList>
-    </SectionInfo>
-  </Wrapper>
-)
+const About = () => {
+  const scrollY = useScrollPosition()
+
+  let inRange
+
+  if (process.browser) {
+    inRange =
+      scrollY > window.innerHeight &&
+      scrollY <
+        window.innerHeight +
+          document.getElementById('about-section-info').clientHeight
+  }
+
+  return (
+    <Wrapper id="about">
+      <Landing inRange={inRange || false}>
+        <Heading>
+          <img
+            src="https://res.cloudinary.com/avatarhzh/image/upload/v1521343685/portfolio/about.svg"
+            alt="About"
+          />
+        </Heading>
+      </Landing>
+      <SectionInfo id="about-section-info">
+        <p>
+          I am a full stack Javascript developer from Wellington, New Zealand.
+        </p>
+        <h3>Tech Stack</h3>
+        <p>
+          I specialise in web development with React and usually work with the
+          MERN stack or React with GraphQL
+        </p>
+        <h3>List of technologies I'm familiar with include:</h3>
+        <TechList>
+          <li>
+            <span>HTML5 and CSS3</span>
+          </li>
+          <li>
+            <span>Git and Github</span>
+          </li>
+          <li>
+            <span>Bootstrap, Material-UI and Semantic UI</span>
+          </li>
+          <li>
+            <span>Sass/Scss, CSS-in-JS and styled-components</span>
+          </li>
+          <li>
+            <span>Javascript and jQuery</span>
+          </li>
+          <li>
+            <span>RxJS</span>
+          </li>
+          <li>
+            <span>Node</span>
+          </li>
+          <li>
+            <span>React</span>
+          </li>
+          <li>
+            <span>Redux, Redux-Thunk and Redux-Observables</span>
+          </li>
+          <li>
+            <span>Next</span>
+          </li>
+          <li>
+            <span>Express3</span>
+          </li>
+          <li>
+            <span>GraphQL, Apollo and Prisma</span>
+          </li>
+          <li>
+            <span>MongoDB and Mongoose</span>
+          </li>
+          <li>
+            <span>PostgreSQL</span>
+          </li>
+          <li>
+            <span>Redis</span>
+          </li>
+        </TechList>
+      </SectionInfo>
+    </Wrapper>
+  )
+}
 
 export default About
